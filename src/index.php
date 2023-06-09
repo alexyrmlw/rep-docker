0fa56e9fa2587fd0296bdf30ec39941d9009a798
alexyrmlw <135985340+alexyrmlw@users.noreply.github.com>
2023-06-09 16:36:40 +0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,9 +3,8 @@
     </head>
       <body>
         <h1>GCP Cloud Build Prod test image</h1>
-          <h2>Test!</h2>
-            <h2>Test2!</h2>
-            <h2>Test3!</h2>
+          <h2>Exam test!</h2>
+        
  <?php
         $ip_server = $_SERVER['SERVER_ADDR'];
   
